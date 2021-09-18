7e4b4b0d7a8db82eae1da6a53b012552a1d8a907
Vreni <vreniseidel492@gmail.com>
2021-09-19 02:08:26 +0500

    Belinda

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, Component} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
+  
+  const onPress = () => {
+    console.log("Function")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={styles.container}>    
+
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Belinda</Text>
+        <TouchableOpacity onPress={onPress} style={styles.bob}>
+          <Text style={styles.wrapperSecondText}>⚙️</Text>
+      </TouchableOpacity>
+      </View>      
+
+      <TouchableOpacity onPress={onPress} style={styles.wrapperFirst}>
+          <Text style={styles.wrapperFirstText}>Text{"\n\n"}</Text>
+          <Text style={styles.wrapperFirstText}>Text</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity onPress={onPress} style={styles.wrapperSecond}>
+          <Text style={styles.wrapperSecondText}>&#10140;</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -14,8 +35,55 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#00FFFF',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+  },
+  sectionTitle: {
+    fontFamily: 'monospace',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  wrapperFirst: {
+    width: 335,
+    height: 500,
+    backgroundColor: '#00FFFA',
+    margin: 20,
+    elevation: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 0.5,
+    borderRadius: 15,
+  },
+  wrapperSecond: {
+    width: 335,
+    height: 100,
+    backgroundColor: '#00FFFA',
+    marginTop: 0,
+    margin: 20,
+    elevation: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 0.5,
+    borderRadius: 15,
+  },
+  wrapperFirstText: {
+    fontFamily: 'monospace',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  wrapperSecondText: {
+    fontFamily: 'monospace',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  bob: {
+    height: 50,
+    paddingLeft: 145
+  }
 });
